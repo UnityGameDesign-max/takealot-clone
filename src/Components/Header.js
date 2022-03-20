a0d8ba4a7e561de513939a700ee133f92ce9d2b5
Tumelo <tumie201511@gmail.com>
2022-03-20 17:49:13 +0200

    updated icons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Logo from "../assets/takealot.png";
-import {HeartFill} from '@styled-icons/bootstrap/HeartFill'
+import { HeartFill } from "@styled-icons/bootstrap/HeartFill";
+import { CartFill } from "@styled-icons/bootstrap/CartFill";
 import styled from "styled-components";
-
-
-
 
 function Header() {
   return (
     <NavBar>
       <NavBarLeft>
         <HeaderLogo></HeaderLogo>
-        <HeaderLink as="a" href="/">Help</HeaderLink>
-        <HeaderLink as="a" href="/">Sell on Takealot</HeaderLink>
+        <HeaderLink as="a" href="/">
+          Help
+        </HeaderLink>
+        <HeaderLink as="a" href="/">
+          Sell on Takealot
+        </HeaderLink>
       </NavBarLeft>
 
       <NavBarRight>
@@ -22,24 +24,44 @@ function Header() {
           <HeaderLink>Orders</HeaderLink>
           <HeaderLink>My Account</HeaderLink>
           <WishListIcon as="a" href="/">
-            <RedZap />
+            <FavIcon />
           </WishListIcon>
+          <CartIcon as="a" href="/">
+            <BasketIcon />
+            
+          </CartIcon>
         </ul>
       </NavBarRight>
     </NavBar>
   );
 }
 
-const RedZap = styled(HeartFill)`
+const FavIcon = styled(HeartFill)`
   color: white;
+  width: 14px;
+`;
+
+const CartIcon = styled.div`
+  padding: 5px 18px;
+  background-color: #1c8644;
+  margin-right: 10px;
+  border-radius: 17px;
+  &:hover {
+    background-color: #1c8644;
+  }
+`;
+const BasketIcon = styled(CartFill)`
   width: 15px;
-`
+  color: white;
+`;
+
 
 const WishListIcon = styled.a`
-  padding: 8px;
+  padding: 6px 8px;
   background-color: #f4697a;
+  margin-right: 10px;
   border-radius: 50%;
-  &:hover{
+  &:hover {
     background-color: #f4697a;
   }
 `;
@@ -49,12 +71,12 @@ const NavBar = styled.div`
   justify-content: space-between;
   margin: 0 auto;
   padding: 12px 60px;
-  max-width: 1440px;
+  max-width: 1240px;
   background-color: white;
 `;
 
 const HeaderLink = styled.a`
-  font-size: 14px;
+  font-size: 13px;
   font-weight: 300;
   margin-inline: 17px;
   color: #4d4d4f;
@@ -77,6 +99,8 @@ const NavBarLeft = styled.div`
   display: flex;
   align-items: center;
 `;
-const NavBarRight = styled.div``;
+const NavBarRight = styled.div`
+ 
+`;
 
 export default Header;
