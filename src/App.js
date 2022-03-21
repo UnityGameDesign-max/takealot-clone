b93c1830b38bbc0b6710d406cf668701d8afd295
Tumelo <tumie201511@gmail.com>
2022-03-21 14:55:37 +0200

    UPDATED APP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import Searching from './Components/Searching';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Searching />
     </div>
   );
 }
