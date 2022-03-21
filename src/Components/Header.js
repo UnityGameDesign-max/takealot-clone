c64435b161d71d3ee7c6a04de84755ea443b66cc
Tumelo <tumie201511@gmail.com>
2022-03-21 11:22:31 +0200

    updated Header basket Icon

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -26,9 +26,9 @@ function Header() {
           <WishListIcon as="a" href="/">
             <FavIcon />
           </WishListIcon>
-          <CartIcon as="a" href="/">
+          <CartIcon as="a" href="#">
             <BasketIcon />
-            
+            <BasketCount>{0}</BasketCount>
           </CartIcon>
         </ul>
       </NavBarRight>
@@ -42,19 +42,27 @@ const FavIcon = styled(HeartFill)`
 `;
 
 const CartIcon = styled.div`
-  padding: 5px 18px;
+  display: flex;
+  align-items: center;
+  padding: 7px 18px;
+  text-decoration: none;
   background-color: #1c8644;
   margin-right: 10px;
   border-radius: 17px;
-  &:hover {
-    background-color: #1c8644;
-  }
 `;
 const BasketIcon = styled(CartFill)`
   width: 15px;
+  margin-right: 4px;
   color: white;
 `;
 
+const BasketCount = styled.p`
+ margin: 0;
+ margin-top:3px;
+ color: white;
+ font-size: 12px;
+ font-weight: bold;
+`
 
 const WishListIcon = styled.a`
   padding: 6px 8px;
@@ -70,8 +78,8 @@ const NavBar = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 0 auto;
-  padding: 12px 60px;
-  max-width: 1240px;
+  padding: 16px 60px;
+  max-width: 1540px;
   background-color: white;
 `;
 
@@ -100,7 +108,11 @@ const NavBarLeft = styled.div`
   align-items: center;
 `;
 const NavBarRight = styled.div`
- 
+ul{
+  display: flex;
+  margin:0;
+  align-items: center;
+}
 `;
 
 export default Header;
