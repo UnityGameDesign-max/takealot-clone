c47511b90d6e21f4775f463daf17d5da0af3af62
Tumelo <tumie201511@gmail.com>
2022-03-22 14:07:45 +0200

    updated utilis for configs of the app

---
+++ src/utilis/navItems.js
@@ -0,0 +1,25 @@
+export const navigationListRight = [
+    {
+        name: "Login",
+        location: "/login"
+    },
+    {
+        name: "Register",
+        location: "/register"
+    },
+    {
+        name: "Orders",
+        location: "/orders"
+    }
+];
+
+export const navigationListLeft = [
+    {
+        name: "Help",
+        location: "/help"
+    },
+    {
+        name: "Sell on Takealot",
+        location: "/sell"
+    }
+]
