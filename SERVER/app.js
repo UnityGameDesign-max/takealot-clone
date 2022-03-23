b8ed31e2b79ca9d45e52c951cf1574ff488a63f9
Lindokuhle shabalala <mlixshaba@gmail.com>
2022-03-23 22:56:27 +0200

    created products table

--- SERVER/app.js
+++ SERVER/app.js
@@ -3,20 +3,23 @@ const app = express();
 const cors = require("cors");
 const path = require("path");
 const port = 5000;
-const { createTable, registerCustomer } = require("./src/db");
+const {
+  createCustomerTable,
+  registerCustomer,
+  createProductsTable,
+} = require("./src/db");
 
-createTable();
+createCustomerTable();
+createProductsTable();
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/account/register", (req, res) => {
   const { firstName, lastName, mobileNumber, email } = req.body;
-  // console.log(firstName, lastName, mobileNumber, email);
+  
   registerCustomer(firstName, lastName, mobileNumber, email).then((data) =>
     res.send(data.rows)
   );
 });
-
-
 
 app.listen(port, () => {
   console.log(`app listening on port: ${port}`);
--- SERVER/src/db.js
+++ SERVER/src/db.js
@@ -7,9 +7,23 @@ client.connect((err) => {
   if (err) throw Error(err.message);
 });
 
-const createTable = () => {
+const createCustomerTable = () => {
   client.query(
     "CREATE TABLE IF NOT EXISTS Customers(ID SERIAL NOT NULL PRIMARY KEY,firstName varchar(50) NOT NULL,lastName varchar(50) NOT NULL,email varchar(50) NOT NULL,mobileNumber INT NOT NULL,newsLetter varchar(50))",
+    (err) => {
+      if (err) {
+        throw Error(err.message);
+      } 
+      setTimeout(() => {
+        client.end();
+      }, 6000000);
+    }
+  );
+};
+
+const createProductsTable = () => {
+  client.query(
+    "CREATE TABLE IF NOT EXISTS Products(ID SERIAL NOT NULL PRIMARY KEY,description varchar(50) NOT NULL, imageLink  varchar(50) NOT NULL, category varchar(50) NOT NULL, price varchar(50) NOT NULL,sale BOOLEAN NOT NULL,percentageOff INT NOT NULL,rating INT NOT NULL,reviews INT NOT NULL)",
     (err) => {
       if (err) {
         throw Error(err.message);
@@ -32,7 +46,19 @@ const registerCustomer = async (firstName, lastName, mobileNumber, email) => {
   }
 };
 
+const addProducts = async (description, imageLink, category, price,sale,percentageOff,rating,reviews) => {
+  try {
+    return await client.query(
+      "INSERT INTO Customers(firstName, lastName, mobileNumber, email) VALUES($1,$2,$3,$4) RETURNING *",
+      [firstName, lastName, mobileNumber, email]
+    );
+  } catch (err) {
+    throw Error(err.message);
+  }
+};
+
 module.exports = {
-  createTable,
+  createCustomerTable,
+  createProductsTable,
   registerCustomer
 };
