9a707f377e2f604c092397d05f40cfb3ae32fb3d
Lindokuhle shabalala <mlixshaba@gmail.com>
2022-03-24 12:54:54 +0200

    creater customer and products routes

---
+++ SERVER/app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const port = 5000;
+const {
+  createCustomerTable,
+  registerCustomer,
+  createProductsTable,
+  addProducts,
+  getAllProducts,
+} = require("./src/db");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/account/register", (req, res) => {
+  createCustomerTable();
+
+    let customer;
+
+  if (!req.body.firstName) {
+    customer = JSON.parse(Object.keys(req.body)[0]);
+  } else {
+    customer = req.body;
+  }
+  const { firstName, lastName, mobileNumber, email } = customer;
+
+  registerCustomer(firstName, lastName, mobileNumber, email).then((data) =>
+    res.send(data.rows)
+  );
+});
+
+app.post("/addProducts", (req, res) => {
+  createProductsTable();
+
+  let product
+
+  if (!req.body.description) {
+    product = JSON.parse(Object.keys(req.body)[0]);
+  } else {
+    product = req.body;
+  }
+
+  const {
+    description,
+    imageLink,
+    category,
+    price,
+    sale,
+    percentageOff,
+    rating,
+    reviews,
+  } = product;
+
+  addProducts(
+    description,
+    imageLink,
+    category,
+    price,
+    sale,
+    percentageOff,
+    rating,
+    reviews
+  ).then((data) => res.send(data.rows));
+});
+
+app.get("/getAllProducts", (req, res) => {
+  getAllProducts().then((data) => res.send(data.rows));
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port: ${port}`);
+});
