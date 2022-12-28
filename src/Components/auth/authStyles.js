c88db66dea22092a15d449b14d1a9bee234fd8ed
Tumelo <tumie201511@gmail.com>
2022-12-29 01:15:40 +0200

    modified auth and add styles to the auth modal

---
+++ src/Components/auth/authStyles.js
@@ -0,0 +1,13 @@
+export const boxModalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    height: 400,
+    bgcolor: 'background.paper',
+    borderRadius: 1,
+    boxShadow: 24,
+    paddingInline: 4,
+    paddingBlock: 2
+};
